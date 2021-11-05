b7c24a6f99431b3bf16cf000250f846c345beeca
Jefferson Boothe <46006304+shamuraii@users.noreply.github.com>
2021-11-05 16:20:45 -0400

    Update data_p.cpp
    Working version with just 1 thread and file-opening changes

--- data_p.cpp
+++ data_p.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 #include <map>
 #include <pthread.h>
 #include <bits/stdc++.h>
@@ -9,11 +8,13 @@
 #include <vector>
 
 using namespace std;
-namespace fs = std::filesystem;
+
+#define NUM_CORES 8;
 
 typedef unordered_map<string, unsigned> StrFreqMap;
 
 typedef struct thread_data {
+    string data_dir;
     vector<string> file_names;
     const char **delwords;
     StrFreqMap freqs;
@@ -23,23 +24,40 @@ void countWords(ifstream &in, StrFreqMap &freqmap, const char *delwords[]);
 
 void *parseThread(void *arg);
 
+void outputSummary(StrFreqMap freqs);
+
 int main() {
-    string data_dir = ".\\certdata";
-    vector<string> file_names;
+    string data_dir = "certdata/";
+    vector<string> file_names =  {
+	    "2013-01-08.txt","2013-01-09.txt","2013-01-11.txt","2013-01-10.txt",
+        "2013-01-25.txt","2013-01-28.txt","2013-01-29.txt","2013-02-01.txt",
+        "2013-02-07.txt","2013-02-14.txt","2013-02-22.txt","2013-03-05.txt",
+        "2013-03-08.txt","2013-03-11.txt","2013-03-18.txt","2013-03-20.txt",
+        "2013-03-21.txt","2013-04-03.txt","2013-04-05.txt","2013-04-02.txt",
+        "2013-04-08.txt","2013-04-10.txt","2013-04-15.txt","2013-04-19.txt",
+        "2013-04-25.txt","2013-04-26.txt","2013-04-29.txt","2013-04-30.txt",
+        "2013-05-06.txt","2013-05-14.txt","2013-05-15.txt","2013-05-17.txt",
+        "2013-06-05.txt","2013-06-07.txt","2013-06-10.txt","2013-06-11.txt",
+        "2013-06-18.txt","2013-06-26.txt","2013-06-27.txt","2013-06-28.txt",
+        "2013-07-12.txt","2013-07-15.txt","2013-07-26.txt","2013-08-01.txt",
+        "2013-08-02.txt","2013-08-05.txt","2013-08-15.txt","2013-08-21.txt",
+        "2013-08-23.txt","2013-08-26.txt","2013-08-30.txt","2013-09-03.txt",
+        "2013-09-04.txt","2013-09-13.txt","2013-09-23.txt","2013-09-24.txt",
+        "2013-10-01.txt","2013-10-04.txt","2013-10-10.txt","2013-10-15.txt",
+        "2013-10-16.txt","2013-10-17.txt","2013-10-18.txt","2013-10-22.txt",
+        "2013-10-25.txt","2013-10-28.txt","2013-10-30.txt","2013-10-31.txt",
+        "2013-11-04.txt","2013-11-07.txt","2013-11-14.txt","2013-11-18.txt",
+        "2013-11-22.txt","2013-12-02.txt","2013-12-03.txt","2013-12-05.txt",
+        "2013-12-12.txt","2013-12-30.txt" };
 
     // List of noise words to delete from list
     const char *delwords[20] = {
         "a", "an", "the", "am", "is", "are", "was", "were", "being",
         "been", "seem", "become", "became", "to", "of", "in", "may", "and", "be", "on" };
 
-    // Help from https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
-    // Create a list of each file in the certdata folder
-    for (const auto & entry : fs::directory_iterator(data_dir)) {
-        file_names.push_back(entry.path().string());
-    }
-
     pthread_t tid;
     thread_data tdata;
+    tdata.data_dir = data_dir;
     tdata.delwords = delwords;
     tdata.file_names = file_names;
 
@@ -48,28 +66,7 @@ int main() {
 
     StrFreqMap freqs = tdata.freqs;
 
-    // Calculate what 10% of the total count is for keeping
-    size_t keep_count = freqs.size() * 0.1;
-    cout << "Keeping " << keep_count << " words." << endl;
-
-    // https://stackoverflow.com/questions/17963905/how-can-i-get-the-top-n-keys-of-stdmap-based-on-their-values
-    // Use partial_sort_copy to extract the top *keep_count* values from the frequency map
-    vector<pair<string, unsigned>> top_ten(keep_count);
-    partial_sort_copy(freqs.begin(),
-                    freqs.end(),
-                    top_ten.begin(),
-                    top_ten.end(),
-                    [](pair<const string, unsigned> const& l,
-                       pair<const string, unsigned> const& r)
-                    {
-                        return l.second > r.second;
-                    });
-    
-    // Display the resulting top 10% and their frequencies
-    cout << "-----------------" << endl;
-    for (auto p : top_ten) {
-        cout << p.first << " : " << p.second << endl;
-    }
+    outputSummary(freqs);
 
     return 0;
 }
@@ -130,15 +127,15 @@ void *parseThread(void *arg) {
     // Loop through each filename
     thread_data *tdata = (thread_data *)arg;
 
+    // Loop through each filename
     for (string fname : tdata->file_names) {
-        // Load the current file
-        ifstream inFile;
-        inFile.open(fname);
+        string path = tdata->data_dir + (fname);
+        ifstream inFile(path.c_str(), ios::in);
 
         // Check for successful open
         if (!inFile.is_open()) {
             cout << fname << " failed to open..." << endl << flush;
-            break;
+            continue;
         }
         
         // Pass the file, frequency map, and noise words to function
@@ -146,5 +143,38 @@ void *parseThread(void *arg) {
 
         inFile.close();
     }
+
     pthread_exit(NULL);
+}
+
+void outputSummary(StrFreqMap freqs) {
+    //declare and open output file
+    ofstream outfile;
+    outfile.open("dp_results.txt");
+
+    // Calculate what 10% of the total count is for keeping
+    size_t keep_count = freqs.size() * 0.1;
+    outfile << "Keeping " << keep_count << " words." << endl;
+
+    // https://stackoverflow.com/questions/17963905/how-can-i-get-the-top-n-keys-of-stdmap-based-on-their-values
+    // Use partial_sort_copy to extract the top *keep_count* values from the frequency map
+    vector<pair<string, unsigned>> top_ten(keep_count);
+    partial_sort_copy(freqs.begin(),
+                    freqs.end(),
+                    top_ten.begin(),
+                    top_ten.end(),
+                    [](pair<const string, unsigned> const& l,
+                       pair<const string, unsigned> const& r)
+                    {
+                        return l.second > r.second;
+                    });
+    
+    // Display the resulting top 10% and their frequencies
+    outfile << "-----------------" << endl;
+    for (auto p : top_ten) {
+        outfile << p.first << " : " << p.second << endl;
+    }
+
+    //close outfile
+    outfile.close();
 }
